37ed61aafb9d632fa69bfc709a96119bc2cf6c55
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-21 11:35:27 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpPageComponent,
+    LoginPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
