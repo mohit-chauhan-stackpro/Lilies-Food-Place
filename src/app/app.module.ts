c7c7625b6bd411cdc350023a4ce2847704d2090e
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-21 14:47:55 +0530

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpPageComponent,
     LoginPageComponent,
-    DashboardComponent
+    DashboardComponent,
+    SideNavComponent,
+    CartComponent,
+    CheckoutComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
