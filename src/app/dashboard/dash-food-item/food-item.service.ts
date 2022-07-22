3280e50e91239dd9ca3ff5fc63b3573efa5db9a8
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-22 12:18:04 +0530

    formated components

--- src/app/dashboard/dash-food-item/food-item.service.ts
+++ src/app/dashboard/dash-food-item/food-item.service.ts
@@ -2,18 +2,67 @@ import { Injectable } from '@angular/core';
 import { FoodItem } from 'src/app/shared/food-item.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodItemService {
-  foodItems:FoodItem[]=[
-    {name:'Stir Fry Pasta',description:'The in-house pasta and chicken by chef Moose',imgPath:'../../../assets/image/dash-food-item-image.png',price:'N 1,000.00',time:'10-20',available:12},
-    {name:'Stir Fry Pasta',description:'The in-house pasta and chicken by chef Moose',imgPath:'../../../assets/image/dash-food-item-image.png',price:'N 1,000.00',time:'10-20',available:12},
-    {name:'Stir Fry Pasta',description:'The in-house pasta and chicken by chef Moose',imgPath:'../../../assets/image/dash-food-item-image.png',price:'N 1,000.00',time:'10-20',available:12},
-    {name:'Stir Fry Pasta',description:'The in-house pasta and chicken by chef Moose',imgPath:'../../../assets/image/dash-food-item-image.png',price:'N 1,000.00',time:'10-20',available:12},
-    {name:'Stir Fry Pasta',description:'The in-house pasta and chicken by chef Moose',imgPath:'../../../assets/image/dash-food-item-image.png',price:'N 1,000.00',time:'10-20',available:12},
-    {name:'Stir Fry Pasta',description:'The in-house pasta and chicken by chef Moose',imgPath:'../../../assets/image/dash-food-item-image.png',price:'N 1,000.00',time:'10-20',available:12},
-    {name:'Stir Fry Pasta',description:'The in-house pasta and chicken by chef Moose',imgPath:'../../../assets/image/dash-food-item-image.png',price:'N 1,000.00',time:'10-20',available:12},
-  ]
+  foodItems: FoodItem[] = [
+    {
+      name: 'Stir Fry Pasta',
+      description: 'The in-house pasta and chicken by chef Moose',
+      imgPath: '../../../assets/image/dash-food-item-image.png',
+      price: 'N 1,000.00',
+      time: '10-20',
+      available: 12,
+    },
+    {
+      name: 'Stir Fry Pasta',
+      description: 'The in-house pasta and chicken by chef Moose',
+      imgPath: '../../../assets/image/dash-food-item-image.png',
+      price: 'N 1,000.00',
+      time: '10-20',
+      available: 12,
+    },
+    {
+      name: 'Stir Fry Pasta',
+      description: 'The in-house pasta and chicken by chef Moose',
+      imgPath: '../../../assets/image/dash-food-item-image.png',
+      price: 'N 1,000.00',
+      time: '10-20',
+      available: 12,
+    },
+    {
+      name: 'Stir Fry Pasta',
+      description: 'The in-house pasta and chicken by chef Moose',
+      imgPath: '../../../assets/image/dash-food-item-image.png',
+      price: 'N 1,000.00',
+      time: '10-20',
+      available: 12,
+    },
+    {
+      name: 'Stir Fry Pasta',
+      description: 'The in-house pasta and chicken by chef Moose',
+      imgPath: '../../../assets/image/dash-food-item-image.png',
+      price: 'N 1,000.00',
+      time: '10-20',
+      available: 12,
+    },
+    {
+      name: 'Stir Fry Pasta',
+      description: 'The in-house pasta and chicken by chef Moose',
+      imgPath: '../../../assets/image/dash-food-item-image.png',
+      price: 'N 1,000.00',
+      time: '10-20',
+      available: 12,
+    },
+    {
+      name: 'Stir Fry Pasta',
+      description: 'The in-house pasta and chicken by chef Moose',
+      imgPath: '../../../assets/image/dash-food-item-image.png',
+      price: 'N 1,000.00',
+      time: '10-20',
+      available: 12,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
--- src/app/shared/food-item.model.ts
+++ src/app/shared/food-item.model.ts
@@ -1,3 +1,10 @@
-export class FoodItem{
-  constructor(public name:string,public description:string,public imgPath:string,public price:string,public time:string,public available:number){}
+export class FoodItem {
+  constructor(
+    public name: string,
+    public description: string,
+    public imgPath: string,
+    public price: string,
+    public time: string,
+    public available: number
+  ) {}
 }
