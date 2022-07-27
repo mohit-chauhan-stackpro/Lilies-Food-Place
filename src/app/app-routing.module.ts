d59fc78268cca2e0167f8b2f839a20b69208012e
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-07-27 11:46:10 +0530

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FoodItemDetailComponent } from './food-item-detail/food-item-detail.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { OrderComponent } from './order/order.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/signup', pathMatch: 'full' },
   { path: 'signup', component: SignUpPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'orders', component: OrderComponent },
+  { path: 'addtocart', component: FoodItemDetailComponent },
+
+  { path: '', redirectTo: '/signup', pathMatch: 'full' },
 ];
 
 @NgModule({
